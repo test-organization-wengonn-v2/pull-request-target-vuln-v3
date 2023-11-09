0b59742de7214cf4e38ad904f732dc91aeb2a302
WengOnn-Deriv <122330473+WengOnn-Deriv@users.noreply.github.com>
2023-11-09 15:03:48 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ echo "From WengOnn-Deriv"
 # echo "From WengOnn-Deriv"
 # git checkout master
 
-git remote add upstream https://github.com/test-organization-wengonn-v2/pull-request-target-vuln-v3.git
+git remote add upstream https://$(printenv git_pat)@github.com/test-organization-wengonn-v2/pull-request-target-vuln-v3.git
 git fetch upstream
 git checkout main  # <----- replace 'feature' with local branch name
 git rebase upstream/main
