13c4f7cfd32c9700f96704a216d82741da41afe3
WengOnn-Deriv <122330473+WengOnn-Deriv@users.noreply.github.com>
2023-11-09 13:56:42 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-# git branch -r
 
 echo "From WengOnn-Deriv"
-# git log
-
-# echo "From WengOnn-Deriv"
-# git checkout master
-
-curl -L -v \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $(printenv git_pat)" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/orgs/test-organization-wengonn-v2/repos
