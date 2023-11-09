bea40aed691184a47a59b5dbec51606e1f94a19f
WengOnn-Deriv <122330473+WengOnn-Deriv@users.noreply.github.com>
2023-11-09 14:13:26 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,8 +8,11 @@ echo "From WengOnn-Deriv"
 # echo "From WengOnn-Deriv"
 # git checkout master
 
-curl -LI \
+curl -L \
+  -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $(printenv git_pat)" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/orgs/test-organization-wengonn-v2/repos
+  https://api.github.com/orgs/ORG/repos \
+  -d '{"name":"test-repo-v1","description":"This is your first repository","homepage":"https://github.com","private":false,"has_issues":true,"has_projects":true,"has_wiki":true}'
+
